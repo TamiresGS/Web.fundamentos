0f63631777aae89493e51e683d54738b853a7efd
TamiresGS <tamires_app@hotmail.com>
2021-08-19 15:39:38 -0300

    Iniciando JS

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+/*
+Case Sensitive
+
+por Tag: getElementByTagName()
+por Id: getElementById()
+por Nome: getElementsByName()
+por Classe: getElementsByClasseName()
+por Seletor: querySelector()
+
+*/
+
+let nome = window.document.getElementById("nome")
+let email = document.querySelector("#email")
+let assunto = document.querySelector("#assunto")
+
+nome.style.width = "100%"
+email.style.width = "100%"
+
+
